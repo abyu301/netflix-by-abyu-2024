ce0d0f2f8722f46f7ee4dd3c28b84d8134d39efb
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2024-01-25 12:12:42 -0700

    .env

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "19630d98570e86a2d1ae1112a8969bc7";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
